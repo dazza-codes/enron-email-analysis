9b1f1a89f3040603fe3a046b063c4a54e8e49505
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2018-01-17 09:33:19 -0800

    Spark Analysis - WIP

--- src/main/scala/SparkAnalysisApp.scala
+++ src/main/scala/SparkAnalysisApp.scala
@@ -1,4 +1,6 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
 import java.nio.file.Paths
 import java.sql.{Connection, Statement}
 //import java.util.Properties
@@ -19,11 +21,6 @@ object SparkAnalysisApp {
 
 //    val jdbcURL = "jdbc:mysql://localhost:3306/enron_email_records?user=enron&password=enronPass"
 //    val connection: Connection = DaoUtil.getConnection(jdbcURL)
-//    connection.isClosed
-//
-//    val statement: Statement = connection.createStatement()
-//    statement.setQueryTimeout(30)
-
 
     if (args.length == 0) {
       throw new IllegalArgumentException("Input Parquet file is required")
@@ -32,18 +29,74 @@ object SparkAnalysisApp {
     val pqPath = Paths.get(args(0)).toAbsolutePath.toString
 
     val spark = SparkSession.builder.appName("Enron-Email-Analysis").getOrCreate()
+    import spark.implicits._
 
     val mailDf = spark.read.parquet(pqPath)
 
     val email_count = mailDf.count
     println(s"Emails loaded: $email_count")
 
-    // Populate the User table
-    for(row <- mailDf.select("from").distinct) {
+
+    /**
+      * Email Users
+      */
+
+    val from = mailDf.select("from").distinct.collect
+    from.foreach( row => {
       val email = row(0).toString
       println(email)
-//      statement.executeUpdate(s"insert into users values('$email')")
-    }
+    })
+
+//    dbutils.fs.rm("/tmp/enron-users.parquet", true)
+    mailDf.select("from")
+      .distinct
+      .write.parquet("enron-users.parquet")
+
+    // Populate the User table  -  not working
+    // Exception in thread "main" org.apache.spark.SparkException: Task not serializable
+//    mailDf.select("from").distinct.foreachPartition ( row_it => {
+//      while(row_it.hasNext) {
+//        val row = row_it.next
+//        val email = row(0).toString
+//        val statement: Statement = connection.createStatement()
+//        statement.setQueryTimeout(30)
+//        statement.executeUpdate(s"insert into users values('$email')")
+//        println(email)
+//      }
+//    })
+
+
+    /**
+      * Email Threads
+      */
+
+    // From Klimpt & Yang paper, section 5:
+    //We have also briefly analyzed the nature of email threads in this corpus. For
+    //this analysis, membership in a thread was determined by two factors. Emails
+    //were considered to be in the same thread if they contained the same words in
+    //their subjects and they were among the same users (addresses).
+
+    // a thread-match algorithm:
+    // - tokenize the words in subject
+    // - find all emails where the intersection of their subject words is "meaningful"
+    // - the word intersection could result in a count and use a "meaningful" threshold (% of words in common?)
+
+    val selection = mailDf
+      .select("from", "subject", "dateEpochUTC")
+      .filter("subject != ''")
+      .orderBy(asc("dateEpochUTC"))
+
+    val tokenSubject = selection.select("subject")
+      .map { row => {
+        row(0).toString
+          .replaceAll("(?i)RE:","")
+          .replaceAll("(?i)FW:","")
+          .split("[ ,;-]+")
+          .filter(_.nonEmpty)
+      }}
+
+    //  user defined function to group by subject?
+    //  h(rdd.keyBy(f).groupByKey().mapValues(g).collect())
 
     spark.stop()
   }
--- src/main/scala/SparkAnalysisScript.scala
+++ src/main/scala/SparkAnalysisScript.scala
@@ -3,14 +3,70 @@ import org.apache.spark.sql.SparkSession
 
 object SparkAnalysisScript extends App {
 
-  // This is just collection of useful spark-shell commands;
-  // it's commented out so it will compile
+  // This is a collection of useful spark-shell commands;
+  // it's commented out so it will not compile
 
 //  val mailDf = spark.read.parquet("enron_email_records.parquet")
 //  println(mailDf.count)
 //  for(row <- mailDf.select("uuid").distinct) { println(row) }
 //  for(row <- mailDf.select("from").distinct) { println(row) }
 
+
+//  val subject = mailDf.select("subject","dateEpochUTC").distinct.collect
+//
+//  val subject = mailDf.select("subject").distinct.collect
+//
+//  subject(0)(0).toString.replaceAll("(?i)RE:","").trim
+//  subject(0)(0).toString.replaceAll("(?i)FW:","").trim
+//
+//  // self join ?
+//  df.as("df1").join(df.as("df2"), $"df1.foo" === $"df2.foo")
+
+
+  // Some messages have no subject - discard those
+
+  // From Klimpt & Yang paper, section 5:
+  //We have also briefly analyzed the nature of email threads in this corpus. For
+  //this analysis, membership in a thread was determined by two factors. Emails
+  //were considered to be in the same thread if they contained the same words in
+  //their subjects and they were among the same users (addresses).
+
+//  val selection = mailDf.orderBy(asc("dateEpochUTC")).select("from", "subject", "dateEpochUTC", "body").collect
+//  val it = selection.iterator
+//  while(it.hasNext) {
+//    val item = it.next
+//    val email = item(0).toString
+//    val subject = item(1).toString
+//    val date = item(2).asInstanceOf[Long]
+//    val body = item(3).toString
+//
+//    // modify the subject to extract useful words
+//
+//    // find all emails that contain the same subject (as a substring)
+//    val matches = mailDf.filter(s"subject like '%$subject%'").collect
+//
+//    // would be good here to say hey, these all belong to a "thread" and then
+//    // remove all these emails from the dataframe so they can't match any other
+//    // threads (assume that every email belongs to only one thread).  By odering the
+//    // messages by date, all the remaining messages should be part of the thread started
+//    // by the current item (the corpus does not begin at time-zero, some early messages are part
+//    // of a threads that were begun before the corpus was collected). Removing
+//    // any of the matching messages from the dataframe should probably also entail removing them
+//    // from the iterator too (but that's nasty stuff, might blow up).
+//    // Considering assignment of message-ID to keys in a MAP and just adding "matches"
+//    // to the values of that map; a later reduction must group all the message keys by
+//    // the "similarity" of their values (e.g. how many words or subjects they have in common?).
+//
+//    // a more sophisticated thread-match, in case subjects are changed during a thread:
+//    // - find all emails where the intersection of their subject words is "meaningful"
+//    // - the word intersection could result in a count and use a "meaningful" threshold (% of words in common?)
+//
+
+  //  user defined function to group by subject?
+  //  h(rdd.keyBy(f).groupByKey().mapValues(g).collect())
+
+
+//  }
 }
 
 
